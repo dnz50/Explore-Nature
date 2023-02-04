3f69cc20a7ac2eed3d43f2cc73f164bc70e97039
dnz50 <akdnz50@gmail.com>
2023-02-04 13:58:27 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#  Explore The Nature
+
+## Outdoor products shoping web page and Nature Blog
+
+ ## Used technologies
+
+ HTML CSS3 Bootstrap JavaScript
+
+ Responsive Page
+ 
+
+ ## WEB-PAGE-GIF
+
+![Alt text](images/ekran.gif)
+
+## you can inspect the web page:
+
+[Web-Page: Explore The Nature netlify.app/]()
---
+++ card.js
@@ -0,0 +1,54 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener('click', () => {
+    item.scrollLeft += containerWidth;
+  })
+
+  preBtn[i].addEventListener('click', () => {
+    item.scrollLeft -= containerWidth;
+  })
+})
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 10 ||
+    document.documentElement.scrollTop > 10
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// let navlinks = document.getElementById("navlinks");
+// function showMenu() {
+//   navlinks.style.right = "-50px"
+
+// }
+// function hideMenu() {
+//   navlinks.style.right = "-200px"
+
+// }
+
